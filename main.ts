587b57ef04b901924c96cd00fdd748bfe11e6ded
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-02 13:19:11 -0400

    56789

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@
 
 // variables
 let neopixelStrip: neopixel.Strip = null
+let loopcounter = 4
 
 // setup
 basic.clearScreen()
@@ -19,44 +20,19 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-
-    // on countdown 4 light up Red
-    basic.showString('4')
+    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
-    pause(1000)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    loopcounter = 4
+    while (loopcounter >= 0){
+    basic.showNumber(loopcounter)
+    neopixelStrip.setPixelColor(loopcounter, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
+    basic.pause(500)
+   loopcounter = loopcounter - 1
+    }
 
-// on countdown 3 light up blue
-    basic.showString('3')
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.show()
-    pause(1000)
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-
-// on countdown 2 light up Green
-    basic.showString('2')
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.show()
-    pause(1000)
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-
-// on countdown 1 light up yellow
-    basic.showString('1')
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.show()
-    pause(1000)
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-
-// on countdown 0 light up Purple
-    basic.showString('0')
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.show()
-    pause(1000)
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
 })
