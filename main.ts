93fdbbfcb12a98fbe6f7648380ba3a28f813b642
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-13 17:40:58 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 
 // variables
 let neopixelStrip: neopixel.Strip = null
-let loopcounter = 0
+let loopcounter = 4
 
 // setup
 basic.clearScreen()
@@ -20,19 +20,16 @@ neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  loopcounter = 4
-  neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    while (loopcounter >= 0) {
+        basic.pause(500)
+        basic.showNumber(loopcounter)
+        neopixelStrip.setPixelColor((3 - loopcounter), neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        loopcounter = loopcounter - 1
+    }
 
-  while (loopcounter >= 0) {
-    basic.showNumber(loopcounter)
-    neopixelStrip.show()
-    basic.pause(1000)
-
-    loopcounter = loopcounter - 1
-    neopixelStrip.setPixelColor(loopcounter, neopixel.colors(NeoPixelColors.Black))
-  }
 })
